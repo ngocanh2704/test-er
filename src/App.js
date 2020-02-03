3c045c7182aba86ead14cda92565285e1a20a7cb
ngocanh2704 <ngocanh27041994@gmail.com>
2020-02-03 13:55:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hoc voi cha hanh
         </a>
       </header>
     </div>
